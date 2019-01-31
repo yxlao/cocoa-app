2888eef30567e81929c3aa75dbe8edb55318f512
Yixing Lao <yixing.lao@gmail.com>
2019-01-31 01:20:10 -0800

    handle file at drop

--- AppDelegate.mm
+++ AppDelegate.mm
@@ -44,6 +44,16 @@
     NSString *open3d_viewer_path = @"/Users/ylao/repo/Open3D-Viewer/open3d-viewer";
     NSString *full_command = [NSString stringWithFormat:@"%@ %@", open3d_viewer_path, filename];
     NSLog(full_command);
+
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/bash"];
+    [task setArguments:[NSArray arrayWithObjects:@"-c", full_command, nil]];
+    [task setStandardOutput:[NSPipe pipe]];
+    [task setStandardInput:[NSPipe pipe]];
+    [task launch];
+    [task waitUntilExit];
+    [task release];
+
     return YES;
 }
 
