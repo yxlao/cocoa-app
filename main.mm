0b86ac0621a3058dcb46d5859770c459ae07b5f4
Yixing Lao <yixing.lao@gmail.com>
2019-01-31 00:26:24 -0800

    calls a shell script

--- main.mm
+++ main.mm
@@ -1,5 +1,13 @@
 #import <AppKit/NSApplication.h> // NSApplicationMain
 
 int main(int argc, const char *argv[]) {
-  return NSApplicationMain(argc, argv);
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/bash"];
+    [task setArguments:[NSArray arrayWithObjects:@"/Users/ylao/repo/playground/write_hello_file.sh", nil]];
+    [task setStandardOutput:[NSPipe pipe]];
+    [task setStandardInput:[NSPipe pipe]];
+    [task launch];
+    [task release];
+    return 0;
+    // return NSApplicationMain(argc, argv);
 }
