98ab6c4deada9fcd8250bb2089a8170c21597931
Yixing Lao <yixing.lao@gmail.com>
2019-01-31 01:10:43 -0800

    file handler and export to mylog

--- AppDelegate.mm
+++ AppDelegate.mm
@@ -6,22 +6,34 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
-    NSAppleEventManager *em = [NSAppleEventManager sharedAppleEventManager];
-    [em
-    setEventHandler:self
-    andSelector:@selector(getUrl:withReplyEvent:)
-    forEventClass:kInternetEventClass
-    andEventID:kAEGetURL];
+    // NSAppleEventManager *em = [NSAppleEventManager sharedAppleEventManager];
+    // [em
+    // setEventHandler:self
+    // andSelector:@selector(getUrl:withReplyEvent:)
+    // forEventClass:kInternetEventClass
+    // andEventID:kAEGetURL];
 }
 
-- (void)getUrl:(NSAppleEventDescriptor *)event
-    withReplyEvent:(NSAppleEventDescriptor *)replyEvent
-{
-    // Get the URL
-    NSString *urlStr = [[event paramDescriptorForKeyword:keyDirectObject]
-        stringValue];
+// - (void)getUrl:(NSAppleEventDescriptor *)event
+//     withReplyEvent:(NSAppleEventDescriptor *)replyEvent
+// {
+//     // Get the URL
+//     NSString *urlStr = [[event paramDescriptorForKeyword:keyDirectObject]
+//         stringValue];
 
-    // TODO: Your custom URL handling code here
+//     // TODO: Your custom URL handling code here
+// }
+
+-(BOOL)application:(NSApplication *)sender openFile:(NSString *)filename
+{
+    int fd = creat("/Users/ylao/repo/cocoa-app/my_log.txt",
+                       S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    close(STDERR_FILENO);
+    dup(fd);
+    close(fd);
+    NSLog(@"this will be written to my_log");
+    NSLog(@"%@", filename);
+    return YES;
 }
 
 @end
--- main.mm
+++ main.mm
@@ -1,16 +1,16 @@
 #import <AppKit/NSApplication.h> // NSApplicationMain
 
 int main(int argc, const char *argv[]) {
-    NSTask *task = [[NSTask alloc] init];
-    [task setLaunchPath:@"/bin/bash"];
-    [task setArguments:[NSArray
-                        arrayWithObjects:@"-c",
-                        @"/Users/ylao/repo/Open3D-Viewer/open3d-viewer /Users/ylao/repo/Open3D/examples/TestData/knot.ply", nil]];
-    [task setStandardOutput:[NSPipe pipe]];
-    [task setStandardInput:[NSPipe pipe]];
-    [task launch];
-    [task waitUntilExit];
-    [task release];
-    return 0;
-    // NSApplicationMain(argc, argv);
+    // NSTask *task = [[NSTask alloc] init];
+    // [task setLaunchPath:@"/bin/bash"];
+    // [task setArguments:[NSArray
+    //                     arrayWithObjects:@"-c",
+    //                     @"/Users/ylao/repo/Open3D-Viewer/open3d-viewer /Users/ylao/repo/Open3D/examples/TestData/knot.ply", nil]];
+    // [task setStandardOutput:[NSPipe pipe]];
+    // [task setStandardInput:[NSPipe pipe]];
+    // [task launch];
+    // [task waitUntilExit];
+    // [task release];
+    // return 0;
+    return NSApplicationMain(argc, argv);
 }
