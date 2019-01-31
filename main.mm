b51f99fed2c40378b93162a26f417c9e0bb38a4f
Yixing Lao <yixing.lao@gmail.com>
2019-01-31 00:51:11 -0800

    independeng from write_hello_file.sh

--- main.mm
+++ main.mm
@@ -3,7 +3,9 @@
 int main(int argc, const char *argv[]) {
     NSTask *task = [[NSTask alloc] init];
     [task setLaunchPath:@"/bin/bash"];
-    [task setArguments:[NSArray arrayWithObjects:@"-c", @"/Users/ylao/repo/Open3D-Viewer/open3d-viewer /Users/ylao/repo/Open3D/examples/TestData/knot.ply", nil]];
+    [task setArguments:[NSArray
+                        arrayWithObjects:@"-c",
+                        @"/Users/ylao/repo/Open3D-Viewer/open3d-viewer /Users/ylao/repo/Open3D/examples/TestData/knot.ply", nil]];
     [task setStandardOutput:[NSPipe pipe]];
     [task setStandardInput:[NSPipe pipe]];
 
