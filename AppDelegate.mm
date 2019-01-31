2e7938feef8cf3036657d833cc9af7ecd49baa0f
Yixing Lao <yixing.lao@gmail.com>
2019-01-31 00:03:47 -0800

    paste the code

--- AppDelegate.mm
+++ AppDelegate.mm
@@ -5,7 +5,23 @@
 @synthesize window = _windows;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-  // Insert code here to initialize your application
+    // Insert code here to initialize your application
+    NSAppleEventManager *em = [NSAppleEventManager sharedAppleEventManager];
+    [em
+    setEventHandler:self
+    andSelector:@selector(getUrl:withReplyEvent:)
+    forEventClass:kInternetEventClass
+    andEventID:kAEGetURL];
+}
+
+- (void)getUrl:(NSAppleEventDescriptor *)event
+    withReplyEvent:(NSAppleEventDescriptor *)replyEvent
+{
+    // Get the URL
+    NSString *urlStr = [[event paramDescriptorForKeyword:keyDirectObject]
+        stringValue];
+
+    // TODO: Your custom URL handling code here
 }
 
 @end
